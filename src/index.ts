6f08a79a17feed5d037a5206c1d24c275750921a
Alekmz <amalexmuniz@gmail.com>
2026-03-12 20:27:02 -0300

    feat: config prisma client

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,21 @@
 import express from 'express';
-
+import { prisma } from './prisma/prisma.ts';
 const app = express();
 
 const port = 3000;
 
 app.get('/', (req, res) => {
-    console.log(req)
-    res.send("Hello world")
+  console.log(req)
+  res.send("Hello world")
+})
+
+// Endpoints usuario
+app.get('/usuarios', (req, res) => {
+  const usuarios = prisma.usuario.findMany();
+  res.json(usuarios);
 })
 
 
-app.listen(port, ()=>{
-  console.log("Servidor ta de pé :p")  
+app.listen(port, () => {
+  console.log("Servidor ta de pé :p")
 })
